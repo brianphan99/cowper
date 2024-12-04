4a34f93d75285fe83e1f2ae0b15d7dc9d0a058c9
KLZX121 <kenneth121lzx@gmail.com>
2024-12-04 14:28:33 +1100

    add csv parsing and date formatting for sportsbet

--- src/sportsbetScrape.py
+++ src/sportsbetScrape.py
@@ -1,5 +1,7 @@
 from scraper import get_html
 import re
+import csv
+from datetime import datetime
 
 """
 Return value:
@@ -18,9 +20,23 @@ matches = [
 ]
 """
 
+def formatDate(dateStr):
+    dateArr = dateStr.split(' ')
+    dateArr.pop(0)
+
+    dayStr = dateArr[0].zfill(2)
+    monthStr = dateArr[1]
+    yearStr = dateArr[2] if len(dateArr) > 2 else datetime.now().year
+
+    monthNum = str(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].index(monthStr) + 1).zfill(2)
+
+    formattedDate = f"{dayStr}/{monthNum}/{yearStr}"
+
+    return formattedDate
+
 # gets sportsbet matches and returns object of odds and dates
 def parseSportsbet(url ='https://www.sportsbet.com.au/betting/soccer'):
-    html = get_html(url)
+    html = get_html(url, 1)
 
     # find the container for all the matches (separated into days (divs))
     daysContainer = html.find('div', {'data-automation-id': 'class-featured-events-container'})
@@ -32,7 +48,7 @@ def parseSportsbet(url ='https://www.sportsbet.com.au/betting/soccer'):
         matchDay = {}
 
         dayDate = day.findChildren(recursive=False)[0].get_text(strip=True)
-        matchDay['date'] = dayDate
+        matchDay['date'] = formatDate(dayDate)
         matchDay['matches'] = []
 
         # get individual matches
@@ -53,3 +69,18 @@ def parseSportsbet(url ='https://www.sportsbet.com.au/betting/soccer'):
         matches.append(matchDay)
 
     return(matches)
+
+matches = parseSportsbet()
+
+# Open a file for writing \
+with open('./src/matchData/sportsbetMatches.csv', 'w', newline='') as csvfile: 
+    csvwriter = csv.writer(csvfile) 
+    # Write header row 
+    csvwriter.writerow(['Date', 'Time', 'Team1', 'Odds1', 'Outcome', 'Draw Odds', 'Team2', 'Odds2'])
+
+    # Write data rows
+    for match_day in matches: 
+        for match in match_day["matches"]: 
+            if len(match["odds"]) != 6: continue
+            row = [match_day["date"], match["time"], match["odds"][0], match["odds"][1], 'Draw', match["odds"][3], match["odds"][4], match["odds"][5]]
+            csvwriter.writerow(row)
