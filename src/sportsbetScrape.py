1e680f84e04e7d9e61679aa642ed6c128ef64827
KLZX121 <kenneth121lzx@gmail.com>
2024-12-10 01:57:23 +1100

    restructure csv output and simplify datetime scrape

--- src/sportsbetScrape.py
+++ src/sportsbetScrape.py
@@ -3,40 +3,9 @@ import re
 import csv
 from datetime import datetime
 
-"""
-Return value:
-
-matches = [
-    {
-    "date": "Thursday, 5 Dec",
-    "matches": [
-        {
-        "time": "05:00",
-        "odds": ["Bologna", "1.70", "Draw", "3.40", "AC Monza", "5.00"]
-        },
-        ...
-    ]},
-    ...
-]
-"""
-
-def formatDate(dateStr):
-    dateArr = dateStr.split(' ')
-    dateArr.pop(0)
-
-    dayStr = dateArr[0].zfill(2)
-    monthStr = dateArr[1]
-    yearStr = dateArr[2] if len(dateArr) > 2 else datetime.now().year
-
-    monthNum = str(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].index(monthStr) + 1).zfill(2)
-
-    formattedDate = f"{dayStr}/{monthNum}/{yearStr}"
-
-    return formattedDate
-
 # gets sportsbet matches and returns object of odds and dates
 def parseSportsbet(sport):
-    url = 0
+    url = None
     if sport == 'soccer':
         url = 'https://www.sportsbet.com.au/betting/soccer'
     elif sport == 'tennis':
@@ -45,8 +14,8 @@ def parseSportsbet(sport):
     html = get_html(url, 1)
 
     # find the container for all the matches (separated into days (divs))
-    daysContainer = 0
-    days = 0
+    daysContainer = None
+    days = None
     if sport == 'soccer':
         daysContainer = html.find('div', {'data-automation-id': 'class-featured-events-container'})
         days = daysContainer.findChildren(recursive=False)
@@ -58,47 +27,44 @@ def parseSportsbet(sport):
     matches = []
 
     for day in days:
-        matchDay = {}
-
-        dayDate = day.findChildren(recursive=False)[0].get_text(strip=True)
-        matchDay['date'] = formatDate(dayDate)
-        matchDay['matches'] = []
-
         # get individual matches
         match_elements = day.find_all('li', class_=re.compile(r'.*cardOuterItem.*'))
+
         for match_element in match_elements:
             match = {}
             # get time of match
-            time = match_element.find('time').get_text(strip=True)
-            match['time'] = time
+            time = match_element.find('time').get('datetime')
+            match['datetime'] = time
 
             # get odds
             odds_element = match_element.find('div', class_=re.compile(r'.*outcomeCardItems.*'))
             odds = odds_element.get_text('|', strip=True).split('|')
             match['odds'] = odds
 
-            matchDay['matches'].append(match)
+            matches.append(match)
         
-        matches.append(matchDay)
 
-    # Open a file for writing \
+    writeToCsv(matches, sport)
+
+    return matches
+
+# writes match data to csv file
+def writeToCsv(data, sport):
     with open(f'./src/matchData/sportsbet{sport}.csv', 'w', newline='') as csvfile: 
         csvwriter = csv.writer(csvfile)
 
         # Write header row 
-        soccerHeaders = ['Date', 'Time', 'Team1', 'Odds1', 'Draw', 'Draw Odds', 'Team2', 'Odds2']
-        tennisHeaders = ['Date', 'Time', 'Player1', 'Odds1', 'Player2', 'Odds2']
+        soccerHeaders = ['datetime', 'team1', 'odds1', 'draw', 'oddsdraw', 'team2', 'odds2']
+        tennisHeaders = ['datetime', 'player1', 'odds1', 'player2', 'odds2']
         
         header = soccerHeaders if sport == 'soccer' else tennisHeaders
         csvwriter.writerow(header)
 
         # Write data rows
-        for match_day in matches: 
-            for match in match_day["matches"]: 
-                if len(match["odds"]) != (len(header) - 2): continue
-                row = [match_day["date"], match["time"]] + match['odds']
-                csvwriter.writerow(row)
+        for match in data: 
+            if len(match["odds"]) != (len(header) - 1): continue
+            row = [match["datetime"]] + match['odds']
+            csvwriter.writerow(row)
 
-    return matches
 
-matches = parseSportsbet('tennis')
+parseSportsbet('soccer')
